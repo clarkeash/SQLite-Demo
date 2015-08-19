ef4baee99c7bc5a21824735743af3a5bc9e10746
Ashley Clarke <me@ashleyclarke.me>
2015-08-19 21:29:33 +0200

    added in loggedin message

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -15,6 +15,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('home', function () {
+    return "Hello " . Auth::user()->name;
+});
+
 Route::controllers([
 	'auth' => 'Auth\AuthController',
 ]);
